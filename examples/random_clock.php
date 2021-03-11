1832a858c4211cec7ff69e0dc58f636031b36097
Jérôme Gamez <jerome@gamez.name>
2021-03-12 00:31:08 +0100

    Add examples outputs

--- examples/random_clock.php
+++ examples/random_clock.php
@@ -8,3 +8,19 @@ $clock = new Clock\RandomClock();
 
 dump($clock->now());
 dump($clock->now());
+
+/*
+❯ php examples/random_clock.php
+^ Safe\DateTimeImmutable @976973969^ {#11
+  -innerDateTime: DateTimeImmutable @976973969 {#8
+    date: 2000-12-16 13:39:29.0 UTC (+00:00)
+  }
+  date: 2000-12-16 13:39:29.0 UTC (+00:00)
+}
+^ Safe\DateTimeImmutable @313983011^ {#21
+  -innerDateTime: DateTimeImmutable @313983011 {#8
+    date: 1979-12-14 01:30:11.0 UTC (+00:00)
+  }
+  date: 1979-12-14 01:30:11.0 UTC (+00:00)
+}
+*/
--- examples/timezone_aware_clock.php
+++ examples/timezone_aware_clock.php
@@ -11,3 +11,25 @@ $newYork = new Clock\TimeZoneAwareClock(new DateTimeZone('America/New_York'));
 
 dump($berlin->now());
 dump($newYork->now());
+
+dump($berlin->now()->getTimestamp());
+dump($newYork->now()->getTimestamp());
+
+/*
+
+❯ php examples/timezone_aware_clock.php
+^ Safe\DateTimeImmutable @1615505397^ {#9
+  -innerDateTime: DateTimeImmutable @1615505397 {#10
+    date: 2021-03-12 00:29:57.265483 Europe/Berlin (+01:00)
+  }
+  date: 2021-03-12 00:29:57.265483 Europe/Berlin (+01:00)
+}
+^ Safe\DateTimeImmutable @1615505397^ {#9
+  -innerDateTime: DateTimeImmutable @1615505397 {#10
+    date: 2021-03-11 18:29:57.270550 America/New_York (-05:00)
+  }
+  date: 2021-03-11 18:29:57.270550 America/New_York (-05:00)
+}
+^ 1615505397
+^ 1615505397
+*/
